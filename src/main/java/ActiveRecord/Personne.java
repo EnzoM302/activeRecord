b064719e5cac2901fc2636e6f932176302753d22
Enzo <enzo2.marchal@gmail.com>
2025-11-25 14:57:46 +0100

    Ajout de test

--- src/main/java/ActiveRecord/Personne.java
+++ src/main/java/ActiveRecord/Personne.java
@@ -54,4 +54,36 @@ public class Personne {
         return new Personne (rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"));
     }
 
+    public ArrayList<Personne>getPersonne() throws SQLException, ClassNotFoundException {
+        ArrayList<Personne> personne = findAll();
+        return personne;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String toString() {
+        try {
+            ArrayList<Personne> personnes = getPersonne();
+            String result = "";
+            for (Personne p : personnes) {
+                result += ("["+p.getId() + "/"+ p.getNom() + "/" + p.getPrenom() + "]");
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
