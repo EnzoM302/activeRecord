aaae420f698f7d7ac712fffcb607198b6b168dd4
Enzo <enzo2.marchal@gmail.com>
2025-11-25 14:34:38 +0100

    Ajout de la methode findByID

--- src/main/java/ActiveRecord/Personne.java
+++ src/main/java/ActiveRecord/Personne.java
@@ -1,5 +1,10 @@
 package ActiveRecord;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Personne {
     private int id = -1;
     private String nom;
@@ -10,6 +15,16 @@ public class Personne {
         this.nom = nom;
         this.prenom = prenom;
     }
+
+    public static Personne findById(int id) throws SQLException, ClassNotFoundException {
+        Connection connet = DBConnection.getConnection();
+        PreparedStatement ps = connet.prepareStatement("SELECT * FROM personne WHERE id = ?");
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        Personne p = new Personne(id, rs.getString("nom"), rs.getString("prenom"));
+        return p;
+    }
+
 
     
 }
