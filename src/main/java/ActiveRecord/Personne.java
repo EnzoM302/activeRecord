341d9d35b368ad37d4bf0e42a2fda684776b6356
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2025-11-25 14:34:03 +0100

    ajout d emeethode

--- src/main/java/ActiveRecord/Personne.java
+++ src/main/java/ActiveRecord/Personne.java
@@ -1,5 +1,8 @@
 package ActiveRecord;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Personne {
     private int id = -1;
     private String nom;
@@ -11,5 +14,27 @@ public class Personne {
         this.prenom = prenom;
     }
 
-    
+    public static ArrayList<Personne> findAll() throws SQLException, ClassNotFoundException {
+        ArrayList<Personne> personne = new ArrayList<>();
+        Connection dbc = DBConnection.getConnection();
+        PreparedStatement pst = dbc.prepareStatement("SELECT * FROM personne");
+        ResultSet rs = pst.executeQuery();
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+            personne.add(new Personne(id, nom, prenom));
+        }
+        return personne;
+    }
+
+    public static Personne findByName(String name) throws SQLException, ClassNotFoundException {
+        Connection dbc = DBConnection.getConnection();
+        PreparedStatement pst = dbc.prepareStatement("SELECT * FROM personne where nom = ?");
+        pst.setString(1,name);
+        ResultSet rs = pst.executeQuery();
+        return new Personne (rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"));
+    }
+
+
 }
